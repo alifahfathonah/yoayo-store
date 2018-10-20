b915dd939a84305c4a922897691f313e8dee74fb
Muhammad Iqbal <iqzenfone.pay@gmail.com>
2018-10-20 19:39:12 +0700

    perbaikan struktur kode

--- app/Http/Controllers/Admin/BerandaController.php
+++ app/Http/Controllers/Admin/BerandaController.php
@@ -21,29 +21,4 @@ class BerandaController extends Controller
         }
 
     }
-
-    public function sidebar_counter() {
-
-        $table = ['barang', 'kategori', 'merk', 'pengguna', 'admin', 'pesanan', 'pembayaran', 'pengiriman'];
-
-        $data = [];
-
-        foreach($table as $key) {
-
-            if($key == 'pengiriman') {
-                $data[] = DB::table('tbl_pesanan')->select('id_pesanan')
-                    ->where('status_pesanan', 3)->count();
-            } else if ($key == 'pesanan') {
-                $data[] = DB::table('tbl_pesanan')->select('id_pesanan')
-                    ->whereBetween('status_pesanan', [1, 2])->count();
-            } else if($key == 'pembayaran') {
-                $data[] = DB::table('tbl_pembayaran')->select('id_pesanan')
-                    ->where('selesai', 0)->count();
-            } else {
-                $data[] = DB::table('tbl_'.$key)->count();
-            }
-        }
-
-        return \json_encode($data, JSON_PRETTY_PRINT);
-    }
 }
